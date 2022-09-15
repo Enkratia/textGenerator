01ed289e48a45042506005fe9cd56aafe030a808
Sergey <manofthetreasure@gmail.com>
2022-09-15 15:12:44 +0300

    m

---
+++ script.js
@@ -0,0 +1,28 @@
+const display = document.querySelector("input");
+const button = document.querySelector("button");
+const copyBtn = document.querySelector("span.far");
+const copyActive = document.querySelector("span.fas");
+
+let chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[];:?><,./-=";
+
+button.onclick = () => {
+
+  randomPassword = "";
+
+  copyBtn.style.display = "block";
+  copyActive.style.display = "none";
+
+  for (i = 0; i < 16; i++) {
+    randomPassword = randomPassword + chars.charAt(
+      Math.floor(Math.random() * chars.length)
+    );
+  }
+  display.value = randomPassword;
+}
+
+function copy() {
+  copyBtn.style.display = "none";
+  copyActive.style.display = "block";
+  display.select();
+  document.execCommand("copy");
+}
